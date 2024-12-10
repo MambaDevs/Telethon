f638b501b31733653a85d26664a65749e4f13f84
MambaDevs <155576521+MambaDevs@users.noreply.github.com>
2024-12-10 15:52:37 +0330

    Update factorization.py
    Reduction of calculation time

--- telethon/crypto/factorization.py
+++ telethon/crypto/factorization.py
@@ -20,32 +20,29 @@ class Factorization:
         """
         if pq % 2 == 0:
             return 2, pq // 2
-
-        y, c, m = randint(1, pq - 1), randint(1, pq - 1), randint(1, pq - 1)
-        g = r = q = 1
-        x = ys = 0
+        y = random.randint(1, pq - 1)
+        c = random.randint(1, pq - 1)
+        g, r, q = 1, 1, 1
 
         while g == 1:
             x = y
-            for i in range(r):
+            y = (pow(y, 2, pq) + c) % pq  
+            for _ in range(r):
                 y = (pow(y, 2, pq) + c) % pq
 
             k = 0
             while k < r and g == 1:
-                ys = y
-                for i in range(min(m, r - k)):
-                    y = (pow(y, 2, pq) + c) % pq
-                    q = q * (abs(x - y)) % pq
-
+                for _ in range(r - k):
+                    y = (y * y + c) % pq
+                    q = (q * abs(x - y)) % pq
                 g = cls.gcd(q, pq)
-                k += m
-
+                k += r
             r *= 2
 
         if g == pq:
             while True:
-                ys = (pow(ys, 2, pq) + c) % pq
-                g = cls.gcd(abs(x - ys), pq)
+                y = (y * y + c) % pq
+                g = cls.gcd(abs(x - y), pq)
                 if g > 1:
                     break
 
